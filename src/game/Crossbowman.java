b993718277a1b27eec333e8e5a19a031c3c97b99
Aleksandr <alexlexx444@yandex.ru>
2022-08-30 13:23:42 +0500

    add Crossbowman class

---
+++ src/game/Crossbowman.java
@@ -0,0 +1,29 @@
+package game;
+
+public class Crossbowman extends BaseHero{
+    private int rangeDamage;
+
+    /**
+     * Args of class:
+     * @param name
+     * @param healPoint
+     * @param damage
+     * @param attack
+     * @param defence
+     * @param speed
+     * @param rangeDamage
+     */
+    public Crossbowman(String name, int healPoint, int damage, int[] attack, int defence, int speed, int rangeDamage) {
+        super(name, healPoint, damage, attack, defence, speed);
+        this.rangeDamage = rangeDamage;
+    }
+
+    public Crossbowman(int healPoint, int damage, int[] attack, int defence, int speed, int rangeDamage) {
+        super("Bowman One", healPoint, damage, attack, defence, speed);
+        this.rangeDamage = rangeDamage;
+    }
+
+    public String getInfo() {
+        return String.format("Class Crossbowman ::: %s", super.getInfo());
+    }
+}
--- src/game/Main.java
+++ src/game/Main.java
@@ -12,7 +12,9 @@ public class Main {
         //System.out.println(snip1.getInfo());
         //Wizard mag1 = new Wizard("Mage One", 30, 17, new int[] {-5, -5}, 12, 9);
         //System.out.println(mag1.getInfo());
-        Sperman sp1 = new Sperman("Spearmn One", 10, 4, new int[] {1, 3}, 5, 4);
-        System.out.println(sp1.getInfo());
+        //Sperman sp1 = new Sperman("Spearmn One", 10, 4, new int[] {1, 3}, 5, 4);
+        //System.out.println(sp1.getInfo());
+        Crossbowman cs1 = new Crossbowman("Csmas One", 10, 12, new int[] {2, 3}, 3, 4, 16);
+        System.out.println(cs1.getInfo());
     }
 }
