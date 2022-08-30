957c47b7f7b85fcdb48da6c4990465a1d659289b
Aleksandr <alexlexx444@yandex.ru>
2022-08-30 11:36:31 +0500

    add Peasant class

--- src/game/Main.java
+++ src/game/Main.java
@@ -4,5 +4,8 @@ public class Main {
     public static void main(String[] args) {
         //BaseHero hero1 = new BaseHero("pere", 100, 10, new int[]{2, 5}, 11, 11);
         //System.out.println(hero1.getInfo());
+        //Peasant heroBrin = new Peasant("Brin", 1, 1, new int[] {1, 1}, 0, 3, true);
+        //System.out.println(heroBrin.getInfo());
+        
     }
 }
--- src/game/Peasant.java
+++ src/game/Peasant.java
@@ -1,4 +1,14 @@
 package game;
 
-public class Peasant {
+public class Peasant extends BaseHero {
+    private boolean delivery;
+
+    public Peasant(String name, int healPoint, int damage, int[] attack, int defence, int speed, boolean delivery) {
+        super(name, healPoint, damage, attack, defence, speed);
+        this.delivery = delivery;
+    }
+
+    public String getInfo() {
+        return String.format("Class Peasant ::: %s", super.getInfo());
+    }
 }
