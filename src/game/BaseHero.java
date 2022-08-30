e27d3c733265fad17d0d17dbfd41edb6205afdba
Aleksandr <alexlexx444@yandex.ru>
2022-08-30 11:25:11 +0500

    add BaseHero class

---
+++ src/game/BaseHero.java
@@ -0,0 +1,48 @@
+package game;
+
+public class BaseHero {
+    private int[] attack = new int[2];
+    private int defence;
+    private int speed;
+    private int damage;
+
+    private int healPoint;
+    private String name;
+
+
+    public BaseHero(String name, int healPoint, int damage, int[] attack, int defence, int speed) {
+        this.name = name;
+        this.damage = damage;
+        this.attack = attack;
+        this.defence = defence;
+        this.speed = speed;
+        this.healPoint = healPoint;
+
+    }
+
+    public BaseHero (int healPoint, int damage, int[] attack, int defence, int speed) {
+        this("Default name", healPoint, damage, attack,defence, speed);
+    }
+
+    public String getInfo() {
+        return String.format("Name %s ::: HP %d", this.name, this.healPoint);
+    }
+
+/*
+    public void healed(int Hp) {
+        this.hp = Hp + this.hp > this.maxHp ? this.maxHp : Hp + this.hp;
+    }
+
+    public void GetDamage(int damage) {
+        if (this.hp - damage > 0) {
+            this.hp -= damage;
+        }
+        // else { die(); }
+    }
+
+    public void Attack(BaseHero target) {
+        int damage = BaseHero.r.nextInt(10, 20);
+        target.GetDamage(damage);
+    }
+*/
+}
---
+++ src/game/Main.java
@@ -0,0 +1,8 @@
+package game;
+
+public class Main {
+    public static void main(String[] args) {
+        //BaseHero hero1 = new BaseHero("pere", 100, 10, new int[]{2, 5}, 11, 11);
+        //System.out.println(hero1.getInfo());
+    }
+}
